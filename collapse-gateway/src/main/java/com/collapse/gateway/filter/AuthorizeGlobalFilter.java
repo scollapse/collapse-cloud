8b123dfec11c4a948b8dfdcf912a7a7859ee4102
shenyunlei2596 <syl2596@hanweb.com>
2023-07-04 14:11:50 +0800

    add:增加工程依赖，新增网关模块，全局鉴权过滤器

---
+++ collapse-gateway/src/main/java/com/collapse/gateway/filter/AuthorizeGlobalFilter.java
@@ -0,0 +1,38 @@
+package com.collapse.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局鉴权过滤器
+ */
+@Component
+public class AuthorizeGlobalFilter implements GlobalFilter , Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1. 获取请求参数
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        // 2. 获取参数判断
+        String authorization = queryParams.getFirst("authorization");
+        if ("admin".equals(authorization)){
+            return chain.filter(exchange);
+        }
+        // 3. 拦截
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
